5aba49c8c608c325775d781ffd0eba3580dc7e35
Jason <jason.acheampong24@gmail.com>
2022-01-03 23:22:49 -0500

    added model inference layout

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app-container">
     <sidenav />
-    <batch-data-container />
-    <graph-container />
+    <batch-data-container v-if="view == 'model info'" />
+    <graph-container v-if="view == 'model info'" />
+    <model-inference v-if="view == 'model inference'"></model-inference>
   </div>
 </template>
 
@@ -10,13 +11,20 @@
 import BatchDataContainer from './components/BatchDataContainer'
 import GraphContainer from './components/GraphContainer'
 import SideNav from './components/SideNav.vue'
+import ModelInference from './components/ModelInference.vue'
 
 export default {
   components: {
     'batch-data-container': BatchDataContainer,
     'graph-container': GraphContainer,
-    'sidenav': SideNav
+    'sidenav': SideNav,
+    'model-inference': ModelInference
   },
+  computed: {
+    view () {
+      return this.$store.state.view
+    }
+  }
 }
 </script>
 
---
+++ src/components/ModelInference.vue
@@ -0,0 +1,97 @@
+<template>
+  <div id="model-inference">
+    <div id="heading">
+      <div id="title">
+        <p>Model Inference</p>
+        <button type="button" class="btn btn-dark">Switch To Cluster</button>
+      </div>
+      <div class="input-group mt-3 mb-3">
+        <span class="input-group-text bg-dark text-white">Titles</span>
+        <input
+         v-model="title1"
+         type="text"
+         class="form-control"
+         placeholder="Title 1"
+         aria-label="Title 1"
+         aria-describedby="button-addon2">
+        <input
+         v-model="title2"
+         type="text"
+         class="form-control"
+         placeholder="Title 2"
+         aria-label="Title 2"
+         aria-describedby="button-addon2">
+        <button class="btn btn-dark" type="button" id="button-addon2" @click="sendTitles()">Compare</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import axios from 'axios'
+import PairTitleInference from './PairTitleInference.vue'
+export default {
+  components: {
+    'pair-title-inference': PairTitleInference
+  },
+  data () {
+    return {
+      title1: '',
+      title2: ''
+    }
+  },
+  methods: {
+    sendTitles () {
+      console.log(`Title 1: ${this.title1} Title 2: ${this.title2}`)
+    }
+  }
+}
+</script>
+
+<style scoped>
+#model-inference {
+  width: 100%;
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  font-family: 'muli';
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 99%;
+  width: 87%;
+  padding-top: 10px;
+  overflow-y: auto;
+  border: 1px solid rgba(222,222,222,0.3);
+  margin-top: 0.3%;
+  border-radius: 15px;
+  -webkit-box-shadow: 10px 10px 8px -10px rgba(222,222,222,1);
+  -moz-box-shadow: 10px 10px 8px -10px rgba(222,222,222,1);
+  box-shadow: 10px 10px 8px -10px rgba(222,222,222,1);
+}
+
+#heading {
+  width: 96%;
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  border-bottom: 1px solid gray;
+}
+
+#title {
+  display: flex;
+  flex-flow: row;
+  justify-content: center;
+}
+
+#title > p {
+  margin: 0;
+  margin-right: 20px;
+  font-size: 24px;
+}
+
+#title > button {
+  margin-left: 20px;
+}
+
+</style>
